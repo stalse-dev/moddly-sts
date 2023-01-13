a58b73df197a848487b49b3f77ffc65fa2d53163
Thiago Bastos <101360634+bastos-stalse@users.noreply.github.com>
2023-01-13 17:48:45 -0300

    Código de atualização de informações do CSS

--- moddly.js
+++ moddly.js
@@ -1,5 +1,6 @@
 window.moddly = window.moddly || {}
 
+//Função que é responsavel por executar a criação do modal
 moddly.open_modal = function(object){
 	moddly.create_modal(object)
 }
@@ -8,76 +9,31 @@ moddly.create_modal = function(object){
 	let moddlyModal = document.createElement('div')
 	moddlyModal.id = 'modal-trial__sts'
 	moddlyModal.classList.add('modal-trial__sts')
-	moddlyModal.setAttribute('style', `display:none; 
-	position: fixed; 
-	z-index: 999; 
-	left: 0;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	overflow: auto;
-	background-color: rgb(0,0,0);
-	background-color: rgba(0,0,0,0.4);
-	display: none;
-	align-items: center;
-	justify-content: center;`)
-
 	moddlyModal.innerHTML = `
-		<div id="modal-content-trial__sts" style="height: 620px;
-			width: 850px;
-			border-radius: 5px;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-between;
-			align-items: center;
-			background-color: #000;">
-
-		<div id="modal-close-trial__btn" style="width: 100%;
-			display: flex;
-			justify-content: flex-end;">
-
-			<div id="close-icon-trial__div" style="margin-right: 10px;
-			margin-top: 10px;
-			width: 30px;
-			height: 30px;
-			padding: 3px;
-			border-radius: 50%;
-			cursor: pointer;
-			background-color: #ffffff;
-			display: flex;
-			justify-content: center;
-			align-items: center;">
-
-				<span id="close-icon-trial__btn" style="font-size: 30px;">&times;</span>
+		<div id="modal-content-trial__sts">
+			<div id="modal-close-trial__btn">
+				<div id="close-icon-trial__div">
+					<span id="close-icon-trial__btn">&times;</span>
+				</div>
 			</div>
-		</div>
 
-		<div id="content-items-trial__sts" style="height: 100%;
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			margin: 0 auto;
-			align-items: center;
-			justify-content: space-around;
-			position: relative;
-			z-index: 2;">
-
-			<div id="thanks-modal__msg" style="width: 80%;
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				justify-content: center;">
-				
+			<div id="content-items-trial__sts">
+				<div id="thanks-modal__msg">
+					<h1 class="main-txt__modal">${object.enp.title}</h1> 
+					<br> <p class="main-txt__modal main-txt__p">${object.enp.content}</p>
+					<br> <img src="${object.enp.img_header}" alt="product">
+				</div>
 			</div>
 		</div>
 	`
 
+	//Parte do código que apenda o CSS no HTML da página
+	let style_modal = document.createElement('style')
+	style_modal.innerText = `${object.enp.style}`
+	document.querySelector('head').appendChild(style_modal)
+
 	document.querySelector('body').appendChild(moddlyModal)
     document.getElementById('modal-trial__sts').style.display = 'flex'
-    var msg = document.getElementById('thanks-modal__msg')
-    msg.innerHTML = `<h1 style="color: #ffff; font-size: 25px;">${object.enp.title}</h1> 
-                    <br> <p style="color: #ffff; font-size: 18px;">${object.enp.content}</p>
-                     <br> <img src="${object.enp.img_header}" alt="product">`
 
 	document.getElementById('close-icon-trial__div').addEventListener('click', function(){
 		document.getElementById('modal-trial__sts').style.display = 'none'
